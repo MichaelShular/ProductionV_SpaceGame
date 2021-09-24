9412d98f5849867cd679bb06e9035226a82569d9
Mick <mickx.shular@gmail.com>
2021-09-23 22:05:51 -0400

    improved movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,61 +6,68 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
     [SerializeField] private float speed;
-    private Vector3 newVel;
+    private Vector3 dir;
+    private float acceleration;
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = this.GetComponent<Rigidbody>();
-        speed = 1000;
+        speed = 1;
     }
 
     // Update is called once per frame
     private void Update()
     {
+
          //Forward
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            newVel += new Vector3(0, 0, 1);
-        }
-        //Backwards
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            newVel += new Vector3(0, 0, -1);
-        }
-        //Strafe Left
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            newVel += new Vector3(-1, 0, 0);
-        }
-        //Strafe Right
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            newVel += new Vector3(1, 0, 0); 
-        }
-        //Roll Left
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    dir += new Vector3(0, 0, 10);
+        //}
+        ////Backwards
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    dir += new Vector3(0, 0, -10);
+        //}
+        ////Strafe Left
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    dir += new Vector3(-5, 0, 0);
+        //}
+        ////Strafe Right
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    dir += new Vector3(5, 0, 0); 
+        //}
+         
+        ////Roll Left
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
 
-        }
-        //Roll Right
-        if (Input.GetKeyDown(KeyCode.E))
-        {
+        //}
+        ////Roll Right
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
 
-        }
-        //Up
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            newVel += new Vector3(0, 1, 0);
-        }
-        //Down
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            newVel += new Vector3(0, -1, 0);
-        }
+        //}
+        ////Up
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    dir += new Vector3(0, 5, 0);
+        //}
+        ////Down
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    dir += new Vector3(0, -5, 0);
+        //}
+
+        dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
     void FixedUpdate()
     {
-       
-        playerRigidbody.velocity = newVel * speed * Time.fixedDeltaTime;
+        if (playerRigidbody.velocity.magnitude <= 20)
+        {
+            playerRigidbody.AddForce((dir * speed) / 2, ForceMode.VelocityChange);
+        }
     }
 }
