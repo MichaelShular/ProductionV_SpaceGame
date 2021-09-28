4d454bb20bc76ea7fc504a180a04c17fe8f3ef05
Mick <mickx.shular@gmail.com>
2021-09-28 13:11:51 -0400

    Added obj layers, bullet obj, bullet timer, bullet movement, player shooting, moved camera to first person

---
+++ Assets/Scripts/Bullet/BulletLifeTime.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLifeTime : MonoBehaviour
+{
+    [SerializeField] float lifeTimeAmount;
+    private float timeLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTimeAmount = 5;
+        timeLeft = lifeTimeAmount + Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft < Time.time)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/Bullet/BulletMovement.cs
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShooting : MonoBehaviour
+public class BulletMovement : MonoBehaviour
 {
+    private Rigidbody rdbody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rdbody = this.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        rdbody.velocity = this.transform.forward * 5 ; 
     }
 }
