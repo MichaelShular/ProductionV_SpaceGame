c7170be928af033add7d23579d0d6f971d22d0c7
Mick <mickx.shular@gmail.com>
2021-09-28 22:51:11 -0400

    Edit controls, added enemy, edit collision for drone

--- Assets/Scripts/Player/BulletSpawn.cs
+++ Assets/Scripts/Player/BulletSpawn.cs
@@ -8,13 +8,13 @@ public class BulletSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(spawningobject);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)  || Input.GetMouseButtonDown(0))
         {
             Instantiate(spawningobject, this.transform);
         }
