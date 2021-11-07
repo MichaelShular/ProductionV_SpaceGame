9364cc9a45d607ba1810d84625370a26584ea64d
Mick <mickx.shular@gmail.com>
2021-11-06 21:56:49 -0500

    Radar works better

--- Assets/Scripts/3DWorldScripts/Player/RadarController.cs
+++ Assets/Scripts/3DWorldScripts/Player/RadarController.cs
@@ -6,7 +6,7 @@ public class RadarController : MonoBehaviour
 {
     [SerializeField] GameObject icon;
     [SerializeField] private Transform playerTransform;
-    private GameObject[] iconInScene;
+    private List<GameObject>  iconInScene;
     private List<Transform> ship;
     // Start is called before the first frame update
     void Start()
@@ -14,24 +14,31 @@ public class RadarController : MonoBehaviour
 
        
         ship = new List<Transform>();
-        iconInScene = new GameObject[5];
-        for (int i = 0; i < iconInScene.Length; i++)
-        {
-            iconInScene[i] = Instantiate(icon);
-            iconInScene[i].transform.parent = transform;
-            iconInScene[i].transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
-        }
+        iconInScene = new List<GameObject>();
+        //for (int i = 0; i < iconInScene.Length; i++)
+        //{
+        //    iconInScene[i] = Instantiate(icon);
+        //    iconInScene[i].transform.parent = transform;
+        //    iconInScene[i].transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
+        //}
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < iconInScene.Length; i++)
+        this.gameObject.transform.forward = Vector3.forward;
+
+        for (int i = 0; i < ship.Count; i++)
         {
-            iconInScene[i].transform.localPosition = ship[i].position / 200 ;
+            
+            iconInScene[i].transform.localPosition = (ship[i].position / 200) - (playerTransform.position/200);
             //Debug.Log(iconInScene[i].transform.localPosition); 
         }
+        //Debug.Log(iconInScene.Count);
+        //Debug.Log(ship.Count);
+
+
         //foreach (Transform a in ship)
         //{
         //    tempIcon.transform.localPosition = Vector3.Normalize(a.position) * 0.5f;
@@ -42,16 +49,28 @@ public class RadarController : MonoBehaviour
     public void addTransform(GameObject a)
     {
         ship.Add(a.transform);
+        GameObject tempIcon = Instantiate(icon);
+        tempIcon.transform.parent = transform;
+        tempIcon.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
+        iconInScene.Add(tempIcon);
     }
 
     public void removeTransform(GameObject a)
     {
         for (int x = 0; 0 < ship.Count; x++)
         {
-            if (a.transform.position == ship[x].position)
+            //Debug.Log(Vector3.Distance(a.transform.position, ship[x].position));
+  
+
+            if (Vector3.Distance(a.transform.position, ship[x].position) < 1)
             {
                 ship.RemoveAt(x);
+                Destroy(iconInScene[x].gameObject);
+                iconInScene.RemoveAt(x);
+                return;
             }
+            
         }
+        
     }
 }
