fe31f7719829bc0db3838c9fbdc9389927e2cd16
Mick <mickx.shular@gmail.com>
2021-11-10 16:58:37 -0500

    Game menu selection

--- Assets/Scripts/GameMissionScripts/GameMissionButtons.cs
+++ Assets/Scripts/GameMissionScripts/GameMissionButtons.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMissionButtons : MonoBehaviour
 {
+    [SerializeField] private GameObject missionPanel;
+    [SerializeField] private GameObject shipPanel;
     public void GamePlaySceneButton()
     {
         SceneManager.LoadScene("3DWorldScene");
@@ -13,6 +16,24 @@ public class GameMissionButtons : MonoBehaviour
     public void BackToMainMenuButtion()
     {
         SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void MissionPanel()
+    {
+        if (shipPanel.activeSelf)
+        {
+            shipPanel.SetActive(!shipPanel.activeSelf);
+        }
+        missionPanel.SetActive(!missionPanel.activeSelf);
+    }
+
+    public void ShipPanel()
+    {
+        if (missionPanel.activeSelf)
+        {
+            missionPanel.SetActive(!missionPanel.activeSelf);
+        }
+        shipPanel.SetActive(!shipPanel.activeSelf);
     }
 
 }
---
+++ Assets/Scripts/GameMissionScripts/ScrollingBackGround.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackGround : MonoBehaviour
+{
+    private float xAndYRotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xAndYRotation += 1 * Time.deltaTime;       
+
+
+        this.transform.rotation = Quaternion.Euler(xAndYRotation,xAndYRotation,0);       
+    }
+}
