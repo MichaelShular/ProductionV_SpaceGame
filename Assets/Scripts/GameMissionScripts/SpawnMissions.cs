d346c5d15d084faf40b7ed8f213b3124ff75243a
Mick <mickx.shular@gmail.com>
2021-12-09 07:13:08 -0500

    fixing mission select menu

--- Assets/Scripts/GameMissionScripts/SpawnMissions.cs
+++ Assets/Scripts/GameMissionScripts/SpawnMissions.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnMissions : MonoBehaviour
 {
     [SerializeField] private GameObject mission;
     public int amountOfMissions;
+    [SerializeField] private GameMissionButtons events;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < amountOfMissions; i++)
         {
-            Instantiate(mission, this.transform);
+            GameObject temp = Instantiate(mission, this.transform);
+            temp.GetComponent<Button>().onClick.AddListener(events.GamePlaySceneButton);
         }
        
     }
